41f215200d983a65743aad584659862e68eec127
Muriel Donzellini <muriel.donzellini@gmail.com>
2020-07-24 17:53:03 -0300

    Adding Typed.JS, Script.JS, Final Touches

---
+++ maravilhosas/muriel-donzellini/js/typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed', {
+    strings: ['Marilena Aparecida Moretti Alves'],
+    /**
+   * @property {number} typeSpeed type speed in milliseconds
+   */
+  typeSpeed: 40,
+    /**
+   * @property {boolean} showCursor show cursor
+   */
+  showCursor: false,
+});
