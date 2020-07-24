15ed9a16517ad8595f930744a10ce2ffe15fce05
Izaira Cunha <izairacunha@gmail.com>
2020-07-24 17:32:49 -0300

    alterações finais

--- maravilhosas/izaira/js/script.js
+++ maravilhosas/izaira/js/script.js
@@ -1,3 +1,31 @@
+(function() {
+  var menuResponsivo = document.getElementById("menuResponsivo");
+  var btnResponsivo = document.getElementById("btnResponsivo");
+
+  btnResponsivo.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.body.scrollTop += 0;
+    if (menuResponsivo.className === "navegacao__itens") {
+      menuResponsivo.className += " ativo";
+    } else {
+      menuResponsivo.className = "navegacao__itens";
+    }
+  });
+})();
+
+window.addEventListener("scroll", function() {
+  if (pageYOffset >= 250) {
+    document.querySelector(".navegacao").style.backgroundColor = "#674582";
+    document.querySelector(".navegacao__itens").style.backgroundColor =
+      "#674582";
+  } else {
+    document.querySelector(".navegacao").style.backgroundColor = "transparent";
+    document.querySelector(".navegacao__itens").style.backgroundColor =
+      "transparent";
+  }
+});
+
+
 
 const debounce = function(func, wait, immediate) {
     let timeout;
@@ -20,7 +48,7 @@ const animateClass = 'animate'
 
 function animeScroll(){
  console.log('scrolando')
- const windowTop = window.pageYOffset + (window.innerHeight * 0.70)
+ const windowTop = window.pageYOffset + (window.innerHeight * 0.80)
  target.forEach(function(e){
      if((windowTop) > e.offsetTop){
          e.classList.add(animateClass)
