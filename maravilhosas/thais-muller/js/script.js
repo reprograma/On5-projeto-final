4db131e5e0c98d8eff04a4e8922925201ea02d84
Thais Muller <thaismullerm@gmail.com>
2020-07-24 22:07:51 -0300

    Adicionando pagina m de maravilhosa

---
+++ maravilhosas/thais-muller/js/script.js
@@ -0,0 +1,60 @@
+(function(){
+
+    var menuResponsivo = document.getElementById("menuResponsivo");
+    var btnResponsivo = document.getElementById("btnResponsivo");
+    
+    btnResponsivo.addEventListener('click', function(e){
+        e.preventDefault();
+        document.body.scrollTop += 0;
+        if (menuResponsivo.className === "navegacao__itens") {
+            menuResponsivo.className += " ativo";
+        } else {
+            menuResponsivo.className = "navegacao__itens";
+        }
+    });
+
+})();
+
+window.addEventListener("scroll", function(){
+   
+    if(pageYOffset >= 250) {
+       document.querySelector('.navegacao').style.backgroundColor = '#674582';
+       document.querySelector('.navegacao__itens').style.backgroundColor = '#674582';
+    } else {
+       document.querySelector('.navegacao').style.backgroundColor = 'transparent';
+       document.querySelector('.navegacao__itens').style.backgroundColor = 'transparent';
+    }
+
+})
+
+
+
+try {
+    const buttonAutora = document.querySelector('.arrow')
+    buttonAutora.addEventListener('click', function(e){
+        e.preventDefault()
+        buttonAutora.classList.toggle('arrow-close')
+        const autoraInfos = document.querySelector('.secao-autora__about')
+        autoraInfos.classList.toggle('opened');
+    })
+} catch {
+    if(!buttonAutora) {
+        console.log('estamos na pagina home')
+    } 
+}
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
+
+
