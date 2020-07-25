9d0a431d7804384cac7e332f37991151372a0730
Raquel Calazans <raqcalazans@gmail.com>
2020-07-24 23:23:55 -0300

    Projeto final - Raquel Calazans

---
+++ maravilhosas/raquel-calazans/js/script.js
@@ -0,0 +1,97 @@
+
+// --- SLIDER ---
+
+//variavel que vai controlar o status do index na array de imagens
+
+let slideIndex = 1;
+
+// é chamada assim que a pagina é carregada, para mostrar o slider
+showSlides(slideIndex);
+
+// funcao que é chamada quando clicamos o botao de passar foto
+// ela chama novamente a funcao de mostrar slides passando um numero que
+// atualiza o slideIndex
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// faz com que quando houver click nas bolinhas ela mudem o slideIndex
+// e mostre a foto referente a bolinha
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    // array de divs com a class mySlides, ou seja, todas as divs de imagens
+    const slides = document.querySelectorAll('.mySlides');
+
+    //array de spans que tem a class dot, ou seja, as bolinhas que
+    // ficam embaixo na foto.
+    const dots = document.querySelectorAll('.dot');
+
+    //controla o slideIndex para nao avançar do length da array
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+     //controla o slideIndex para nao ficar um valor negativo
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].classList.add('active');
+}
+
+// --- SEÇÃO AUTORA ---
+let btnAutora = document.querySelector(".arrow");
+let autoraAbout = document.querySelector(".secao-autora__about");
+
+btnAutora.addEventListener("click", function(){
+    if (autoraAbout.classList.contains("closed")) {
+        autoraAbout.classList.remove("closed");
+        btnAutora.classList.add("arrow-close");
+    } else {
+        autoraAbout.classList.add("closed");
+        btnAutora.classList.remove("arrow-close");
+    }
+});
+
+// --- NAV RESPONSIVA  
+
+(function () {
+    var menuResponsivo = document.getElementById("menuResponsivo");
+    var btnResponsivo = document.getElementById("btnResponsivo");
+
+    btnResponsivo.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.body.scrollTop += 0;
+        if (menuResponsivo.className === "navegacao__itens") {
+            menuResponsivo.className += " ativo";
+        } else {
+            menuResponsivo.className = "navegacao__itens";
+        }
+    });
+})();
+
+window.addEventListener("scroll", function () {
+    if (pageYOffset >= 250) {
+        document.querySelector(".navegacao").style.backgroundColor = "#674582";
+        document.querySelector(".navegacao__itens").style.backgroundColor =
+            "#674582";
+    } else {
+        document.querySelector(".navegacao").style.backgroundColor = "transparent";
+        document.querySelector(".navegacao__itens").style.backgroundColor =
+            "transparent";
+    }
+});
+
+
